01f9d5a8d1523dfee9c79df03bacc3140c473416
Abanoub Ghadban <abanoub.gdb@gmail.com>
2021-06-26 23:19:51 +0200

    added auth protection middlewares

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php include_once (__DIR__ . "/middlewares/auth.php"); ?>
+<?php
+include_once (__DIR__ . "/models/User.php");
+$user = User::get_by_id($user_id);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +15,8 @@
 </head>
 
 <body>
-    <?php echo "hello" ?>
+    <?= "hello $user->name" ?>
+    <a href="logout.php">Logout</a>
 </body>
 
 </html>
